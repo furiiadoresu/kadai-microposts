ce684ee7dd7279729ad103a38bc46451a4857e5e
furiiadoresu <furiiadoresu@gmail.com>
2022-03-06 15:31:01 +0000

    timeline

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]  
-  
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
-    @pagy, @users = pagy(User.order(id: :desc), items: 10)
+    @pagy, @users = pagy(User.order(id: :desc), items: 25)
   end
-  
+
   def show
     @user = User.find(params[:id])
     @pagy, @microposts = pagy(@user.microposts.order(id: :desc))
     counts(@user)
   end
-  
+
   def new
     @user = User.new
   end
@@ -26,7 +26,19 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
+  def followings
+    @user = User.find(params[:id])
+    @pagy, @followings = pagy(@user.followings)
+    counts(@user)
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @pagy, @followers = pagy(@user.followers)
+    counts(@user)
+  end
+
   private
 
   def user_params
